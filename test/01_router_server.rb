e9eece94c780c4d4d77ec856d9f055b7e7dfe7ec
CJ Avilla <cjavilla@gmail.com>
2014-03-11 12:16:53 -0700

    updating router server to move the router to boot time

--- test/01_router_server.rb
+++ test/01_router_server.rb
@@ -30,16 +30,16 @@ class UserController < ControllerBase
   end
 end
 
+router = Router.new
+router.draw do
+  get Regexp.new("^/statuses$"), StatusController, :index
+  get Regexp.new("^/users$"), UserController, :index
+
+  # uncomment this when you get to route params
+  # get Regexp.new("^/statuses/(?<id>\\d+)$"), StatusController, :show
+end
+
 server.mount_proc '/' do |req, res|
-  router = Router.new
-  router.draw do
-    get Regexp.new("^/statuses$"), StatusController, :index
-    get Regexp.new("^/users$"), UserController, :index
-
-    # uncomment this when you get to route params
-#    get Regexp.new("^/statuses/(?<id>\\d+)$"), StatusController, :show
-  end
-
   route = router.run(req, res)
 end
 
